417e8b7402f4d8ac9ce7d3e31fc549b45c331b49
Motoki Wu <apewu@Motokis-MacBook-Pro.local>
2017-02-20 13:14:20 -0800

    1) removed pandas (memory issues) 2) write per line (but only sentences) 3) remove html tags 4) remove square brackets (for links) 5) use .tsv (though not really separated) 6) fix encodings 7) fix weird html encodings 8) fix whitespace issues 9) save as unicode

--- getcomments.py
+++ getcomments.py
@@ -1,54 +1,86 @@
+from __future__ import unicode_literals
+
+import io
+import unidecode
+import ftfy
 import urllib2
 import json
 import datetime
 import time
 import pytz
-import pandas as pd
+# import pandas as pd
 import os
-from pandas import DataFrame
+import re
+# from pandas import DataFrame
 
-filename = 'hacker_news_comments.csv'
+from HTMLParser import HTMLParser  # python 2.x
 
-def update_csv(f, df):
-	df["comment_text"] = df["comment_text"].map(lambda x: x.translate(dict.fromkeys([0x201c, 0x201d, 0x2011, 0x2013, 0x2014, 0x2018, 0x2019, 0x2026, 0x2032])).encode('utf-8').replace(',',''))
-	df["created_at"] = df["created_at_i"].map(lambda x: datetime.datetime.fromtimestamp(int(x), tz=pytz.timezone('America/New_York')).strftime('%Y-%m-%d %H:%M:%S'))
+filename = 'hacker_news_comments.tsv'
 
-	ordered_df = df[["comment_text","points","author","created_at","objectID"]]
+html_parser = HTMLParser()
 
-	ordered_df.to_csv(f,encoding='utf-8', index=False)
+html_tags = re.compile(r'<.*?>')
+square_brackets = re.compile(r'\[.*?\]')
 
+remove_html_tags = lambda x: re.sub(html_tags, " ", x)
+remove_square_brackets = lambda x: re.sub(square_brackets, " ", x)
 
 if os.path.isfile(filename):
     os.remove(filename)
 
 ts = str(int(time.time()))
-df = DataFrame()
+# df = DataFrame()
 hitsPerPage = 1000
 requested_keys = ["author", "comment_text", "created_at_i", "objectID", "points"]
 
 i = 0
 
 while True:
-	with open(filename, 'a') as f:
-		try:
-			url = 'https://hn.algolia.com/api/v1/search_by_date?tags=comment&hitsPerPage=%s&numericFilters=created_at_i<%s' % (hitsPerPage, ts)
-			req = urllib2.Request(url)
-			response = urllib2.urlopen(req)
-			data = json.loads(response.read())
-			last = data["nbHits"] < hitsPerPage
-			data = DataFrame(data["hits"])[requested_keys]
-			df = df.append(data,ignore_index=True)
-			ts = data.created_at_i.min()
-			print i
-			if (last):
-				update_csv(f, df)
-				break
-			if (i % 2 == 0): # We write occasionally
-				update_csv(f, df)
-				df = DataFrame()				
+	with io.open(filename, 'a', encoding='utf8') as f:
+	# try:
+		url = 'https://hn.algolia.com/api/v1/search_by_date?tags=comment&hitsPerPage=%s&numericFilters=created_at_i<%s' % (hitsPerPage, ts)
+		req = urllib2.Request(url)
+		response = urllib2.urlopen(req)
+		data = json.loads(response.read())
+		last = data["nbHits"] < hitsPerPage
+		# data = DataFrame(data["hits"])[requested_keys]
+		# df = df.append(data,ignore_index=True)
+		# ts = data.created_at_i.min()
+		hits = data["hits"]
+		print ("query", i, "n hits", len(hits))
 
-			time.sleep(3.6)
-			i += 1
+		for hit in hits:
+			# import pdb
+			# pdb.set_trace()
+			comment_text = unicode(hit["comment_text"])
+			comment_text = comment_text.strip()
+			comment_text = html_parser.unescape(comment_text)
+			comment_text = remove_html_tags(comment_text)
+			comment_text = remove_square_brackets(comment_text)
+			comment_text = ftfy.fix_text(comment_text)
+			comment_text = unidecode.unidecode(comment_text)
+			# comment_text = comment_text.translate(dict.fromkeys([0x201c, 0x201d, 0x2011, 0x2013, 0x2014, 0x2018, 0x2019, 0x2026, 0x2032]))
+			comment_text = u" ".join(comment_text.split())
+			comment_text = unicode(comment_text)				
 
-		except Exception, e:
-			print e
+			if len(comment_text) == 0:
+				continue
+
+			f.write(comment_text)
+			f.write(u'\n')
+
+		if (last):
+			break
+		# if (i % 2 == 0): # We write occasionally
+			# update_csv(f, df)
+			# df = DataFrame()				
+
+		time.sleep(3.6)
+		i += 1
+
+		# except Exception as e:
+		# 	print ("Exception")
+		# 	import pdb
+		# 	pdb.set_trace()
+		# 	# print (e)
+		# 	raise e
